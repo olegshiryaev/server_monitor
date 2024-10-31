8a23bc436dab985cde96f2a10929cb9fc755cb19
olegshiryaev <oashiryaev@yandex.ru>
2024-10-31 23:37:59 +0300

    celery

---
+++ server_monitor/celery.py
@@ -0,0 +1,17 @@
+import os
+from celery import Celery
+
+# Устанавливаем переменную окружения для настройки Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server_monitor.settings")
+
+# Инициализация Celery приложения
+app = Celery("server_monitor")
+
+# Загрузка конфигурации Celery из настроек Django с префиксом CELERY_
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Автоматическое обнаружение задач из всех зарегистрированных Django приложений
+app.autodiscover_tasks()
+
+# Повторные попытки подключения к брокеру при старте (Celery 6.0+)
+app.conf.broker_connection_retry_on_startup = True
